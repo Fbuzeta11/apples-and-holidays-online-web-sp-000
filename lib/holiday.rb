b4f30714ca23ea47c030da7b0ff0aa55547968c5
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-29 22:44:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,7 @@ end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring].each do |holiday, supplies|
-    supplies << "Grill"
+    supplies << "Grill" << "Table Cloth"
     end
     
   
